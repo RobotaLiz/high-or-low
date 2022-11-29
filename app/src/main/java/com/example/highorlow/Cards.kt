cce3d39252d95feea78f2d66f4762a59bdcb165d
lizahjortling <lizhj90@outlook.com>
2022-11-29 10:19:55 +0100

    put the cards in a class

---
+++ app/src/main/java/com/example/highorlow/Cards.kt
@@ -0,0 +1,20 @@
+package com.example.highorlow
+
+object Cards {
+
+
+    var cards = arrayOf(R.drawable.diamond2, R.drawable.club2,R.drawable.heart2,R.drawable.spade2,
+        R.drawable.diamond3, R.drawable.club3,R.drawable.heart3,R.drawable.spade3,
+        R.drawable.diamond4, R.drawable.club4,R.drawable.heart4,R.drawable.spade4,
+        R.drawable.diamond5, R.drawable.club5,R.drawable.heart5,R.drawable.spade5,
+        R.drawable.diamond6, R.drawable.club6,R.drawable.heart6,R.drawable.spade6,
+        R.drawable.diamond7, R.drawable.club7,R.drawable.heart7,R.drawable.spade7,
+        R.drawable.diamond8, R.drawable.club8,R.drawable.heart8,R.drawable.spade8,
+        R.drawable.diamond9, R.drawable.club9,R.drawable.heart9,R.drawable.spade9,
+        R.drawable.diamond10, R.drawable.club10,R.drawable.heart10,R.drawable.spade10,
+        R.drawable.diamondjack, R.drawable.clubjack,R.drawable.heartjack,R.drawable.spadejack,
+        R.drawable.diamondqueen, R.drawable.clubqueen,R.drawable.heartqueen,R.drawable.spadequeen,
+        R.drawable.diamondking, R.drawable.clubking,R.drawable.heartking,R.drawable.spadeking,
+        R.drawable.diamondace, R.drawable.clubace,R.drawable.heartace,R.drawable.spadeace)
+
+}
--- app/src/main/java/com/example/highorlow/GameBoard.kt
+++ app/src/main/java/com/example/highorlow/GameBoard.kt
@@ -9,20 +9,7 @@ import android.os.CountDownTimer
 
 class GameBoard : AppCompatActivity() {
 
- // diamond,club,heart,spade
-    var cards = arrayOf(R.drawable.diamond2, R.drawable.club2,R.drawable.heart2,R.drawable.spade2,
-     R.drawable.diamond3, R.drawable.club3,R.drawable.heart3,R.drawable.spade3,
-     R.drawable.diamond4, R.drawable.club4,R.drawable.heart4,R.drawable.spade4,
-     R.drawable.diamond5, R.drawable.club5,R.drawable.heart5,R.drawable.spade5,
-     R.drawable.diamond6, R.drawable.club6,R.drawable.heart6,R.drawable.spade6,
-     R.drawable.diamond7, R.drawable.club7,R.drawable.heart7,R.drawable.spade7,
-     R.drawable.diamond8, R.drawable.club8,R.drawable.heart8,R.drawable.spade8,
-     R.drawable.diamond9, R.drawable.club9,R.drawable.heart9,R.drawable.spade9,
-     R.drawable.diamond10, R.drawable.club10,R.drawable.heart10,R.drawable.spade10,
-     R.drawable.diamondjack, R.drawable.clubjack,R.drawable.heartjack,R.drawable.spadejack,
-     R.drawable.diamondqueen, R.drawable.clubqueen,R.drawable.heartqueen,R.drawable.spadequeen,
-     R.drawable.diamondking, R.drawable.clubking,R.drawable.heartking,R.drawable.spadeking,
-     R.drawable.diamondace, R.drawable.clubace,R.drawable.heartace,R.drawable.spadeace)
+
 
     lateinit var scoreView : TextView
     lateinit var cardView : ImageView
@@ -31,6 +18,7 @@ class GameBoard : AppCompatActivity() {
     var hasStarted = false
     lateinit var timerView : TextView
     var backside = (R.drawable.backside)
+
 
     var timer = object: CountDownTimer(30000, 1000) {
          override fun onTick(millisUntilFinished: Long) {
@@ -103,7 +91,7 @@ class GameBoard : AppCompatActivity() {
     }
     fun nextCard (){
         card = (0..52).random()
-        cardView.setImageResource(cards[card])
+        cardView.setImageResource(Cards.cards[card])
     }
     fun giveScore(){
         score++
